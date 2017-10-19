661ae7946fd6a9f28aedbf49530f455e7d312b69
Taylor Lovett <tlovett88@gmail.com>
2017-10-19 11:36:13 -0400

    Fix instructions main domain

--- src/classes/Command/Pull.php
+++ src/classes/Command/Pull.php
@@ -415,7 +415,7 @@ class Pull extends Command {
 					$output->writeln( "define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'localhost');
+define('DOMAIN_CURRENT_SITE', '" . $main_domain . "');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);");
