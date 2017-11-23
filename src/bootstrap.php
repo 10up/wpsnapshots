d189a27d45dd2f5066da7f84bb127093d8116c58
Taylor Lovett <tlovett88@gmail.com>
2017-11-22 23:19:39 -0500

    Start command

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -22,5 +22,6 @@ $app->add( new Command\Push() );
 $app->add( new Command\Pull() );
 $app->add( new Command\Search() );
 $app->add( new Command\Delete() );
+$app->add( new Command\CreateEnvironment() );
 
 $app->run();
