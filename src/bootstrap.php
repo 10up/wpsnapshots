b017a841d15fae96d4696b731a5b2c33f22515a1
Taylor Lovett <tlovett88@gmail.com>
2020-04-13 20:34:16 -0400

    Bump version

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,7 +9,7 @@ namespace WPSnapshots;
 
 use \Symfony\Component\Console\Application;
 
-define( 'WPSNAPSHOTS_VERSION', '1.6.3' );
+define( 'WPSNAPSHOTS_VERSION', '1.7' );
 
 require_once __DIR__ . '/utils.php';
 
