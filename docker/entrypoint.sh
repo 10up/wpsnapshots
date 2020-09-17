17c33145414b29720583904808dc9b05800695e1
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-09-17 21:29:39 +0300

    Updated dockerfile to properly build it.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -d /home/wpsnapshots/.wpsnapshots ]; then
+	www_uid=`stat -c "%u" /home/wpsnapshots/.wpsnapshots`
+	www_gid=`stat -c "%g" /home/wpsnapshots/.wpsnapshots`
+	if [ ! $www_uid -eq 0 ]; then
+		usermod -u $www_uid wpsnapshots 2> /dev/null
+		groupmod -g $www_gid wpsnapshots 2> /dev/null
+	fi
+fi
+
+exec su - wpsnapshots -c "/opt/wpsnapshots/bin/wpsnapshots $*"
