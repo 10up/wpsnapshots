6471bbc58ab450ec1c8bb018f9266692335ffa13
Taylor Lovett <tlovett88@gmail.com>
2021-05-17 12:28:45 -0400

    Update Push.php

--- src/classes/Command/Push.php
+++ src/classes/Command/Push.php
@@ -69,7 +69,7 @@ class Push extends Command {
 			return 1;
 		}
 
-		$output->writeln( '<comment>Security Warning: WPSnapshots creates copies of your codebase and database. This could result in data retention policy issues, please exercise extreme caution when using production data.</comment>' );
+		$output->writeln( '<comment>Security Warning: WP Snapshots creates copies of your codebase and database. This could result in data retention policy issues, please exercise extreme caution when using production data.</comment>' );
 
 		$snapshot_id = $input->getArgument( 'snapshot_id' );
 
